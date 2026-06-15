8011d88f345428ef09fab76226afc9be60e9e04d
yesxp <yesxp@users.noreply.github.com>
2026-06-15 23:22:23 +0800

    refactor: 切换到uni-echarts，移除lime-echart，支持tree-shaking按需加载

--- src/components/LEchart.vue
+++ src/components/LEchart.vue
@@ -1,56 +1,15 @@
 <template>
-  <view class="echarts-wrap" :style="{ width: width, height: height }">
-    <l-echart ref="chartRef" @finished="onFinished" />
-  </view>
+  <UniEcharts
+    :option="option"
+    :custom-style="customStyle"
+  />
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onBeforeUnmount } from 'vue'
+import UniEcharts from 'uni-echarts'
 
-const props = defineProps<{
+defineProps<{
   option: any
-  width?: string
-  height?: string
+  customStyle?: string
 }>()
-
-const chartRef = ref<any>(null)
-let chartInstance: any = null
-let echartsLib: any = null
-
-async function onFinished() {
-  if (!chartRef.value) return
-  // #ifdef H5
-  echartsLib = await import('echarts')
-  // #endif
-  // #ifdef MP-WEIXIN
-  echartsLib = require('../../uni_modules/lime-echart/static/echarts.min')
-  // #endif
-  // #ifndef H5 || MP-WEIXIN
-  echartsLib = await import('echarts')
-  // #endif
-
-  chartInstance = await chartRef.value.init(echartsLib)
-  if (chartInstance && props.option) {
-    chartInstance.setOption(props.option)
-  }
-}
-
-watch(() => props.option, (newOpt) => {
-  if (chartInstance && newOpt) {
-    chartInstance.setOption(newOpt, true)
-  }
-}, { deep: true })
-
-onBeforeUnmount(() => {
-  if (chartInstance) {
-    chartInstance.dispose()
-    chartInstance = null
-  }
-})
 </script>
-
-<style scoped>
-.echarts-wrap {
-  overflow: hidden;
-}
-</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+import { UniEcharts } from 'uni-echarts/vite'
 
 export default defineConfig({
-  plugins: [uni()]
+  plugins: [uni(), UniEcharts()],
 })
